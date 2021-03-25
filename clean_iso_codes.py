330f3d6a10bfdb8885a3160c7d35368d3628161d
lucia-gomez <isabel.lucia.gomez@gmail.com>
2021-03-24 20:44:23 -0400

    automate country flags

---
+++ clean_iso_codes.py
@@ -0,0 +1,16 @@
+'''
+    flip the keys and values from http://country.io/iso3.json
+'''
+
+import json
+
+codes = {}
+with open("country_codes.txt") as f:
+    for line in f.readlines():
+        if len(line) > 2:
+            iso2 = line[3:5]
+            iso3 = line[9:12]
+            codes[iso3] = iso2
+
+with open("iso3_to_iso2.json", "w") as f:
+    f.write(json.dumps(codes))
